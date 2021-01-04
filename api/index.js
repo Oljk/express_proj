acdee91bd9b6c8c2ae0f65acad73c5a936e7a789
Oljk <8Kozyavka8>
2021-01-05 00:25:23 +0200

    add error handlers with logging

--- api/index.js
+++ api/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+function writeLog (err, req, res, next) {
+    console.log('index.js mes: ' + err.message)
+    next(err);
+}
 
 function errorHandler (err, req, res, next) {
-    if (res.headersSent) {
+    if (res.headersSent) { //the app sent HTTP headers for the response
         return next(err)
     }
     res.status(500)
-    res.render('error', { error: err })
+    res.send('error');
+}
+
+function allErrorsHandle (err, req, res, next) {
+    res.status(501)
+    res.send('unknown error, please contact admin');
 }
 
 app.get('/', (req, res) => {
+    const ok = Boolean(Math.round(Math.random()));
+    if (!ok) {
+        throw  new Error('Random Math error at get / '); // ошибка будет валится рандомно
+    }
     res.send('Hello World!')
 })
 
@@ -18,5 +31,7 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.use(errorHandler);
+app.use(writeLog); // сначала в лог
+app.use(errorHandler); // при падении ошибки обработает
+app.use(allErrorsHandle);
 
