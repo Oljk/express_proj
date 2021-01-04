f59d3b2bfb74d45bf29efafc83e59d9456d93b60
Oljk <8Kozyavka8>
2021-01-05 00:05:03 +0200

    new branch for the task

--- api/index.js
+++ api/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -9,3 +17,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.use(errorHandler);
+
